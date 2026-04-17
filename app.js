565fb1b39dec78592277701b19a2aa7e76f61b90
aplanas75-creator <aplanas75@gmail.com>
2026-04-17 18:54:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,4 @@ const msg = `Hola, quiero revisar mi factura de luz. Ref: ${ref}`;
 
 document.getElementById("wa").href =
   `https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
+<p>⚠️ La mayoría de hogares paga más de lo necesario sin saberlo</p>
